a070390ca46d188bf72564197acdb713887891b7
Catherine Tan <tan.l.catherine@gmail.com>
2020-05-13 11:03:00 -0700

    add page

---
+++ cat-com/pages/resume/index.js
@@ -0,0 +1,23 @@
+
+export default function Resume() {
+  return (
+    <div className="container">
+      <main>
+        <iframe src="https://docs.google.com/document/d/e/2PACX-1vRsEdLSnm64NeoVn5Dpjg0YnyGbKCmvsrJTkTTY6wzJl04PkuT_1vLiFgxH06MOUPz1oihN3lUluwIc/pub?embedded=true"></iframe>      </main> 
+      <style jsx>{`
+      iframe {
+        margin: 0 auto;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        border: 0;
+        width: 845px;
+        left: 0;
+        right: 0;
+      }
+      `}</style>
+    </div>
+  );
+  
+}
